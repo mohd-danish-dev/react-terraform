f0b20406093e654ac7776df79d0834709f8029dc
mohd-danish-dev <85397196+mohd-danish-dev@users.noreply.github.com>
2023-10-29 20:42:21 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,17 +1,14 @@
 variable "region" {
   description = "AWS region where infrastructure is going to be created."
-  default     = "ap-south-1"
   type        = string
 }
 
 variable "project" {
   description = "Project name"
-  default     = "react-terraform-sample"
   type        = string
 }
 
 variable "env" {
   type        = string
-  default     = "dev"
   description = "defines the running environment"
 }
